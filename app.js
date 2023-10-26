5e51845cdf60d07975dcc6df6e7c30f4b40b3427
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-10-26 12:29:07 -0300

    :construction: feat: feita conexão com o back

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.use(cors());
 
 app.use(express.json());
 
+// DB Connection
+const conn = require("./db/conn");
+
+conn();
+
 app.listen(3000, function () {
   console.log("### Servidor Online ###");
 });
