a5844583e881e09a75cd8ae22300933f0121c89c
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-11-15 11:15:09 -0300

    :sparkles: feat: feitos métodos de put e delete nas mesas

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -41,6 +41,41 @@ const groupController = {
       console.log(`Erro: ${error}`);
     }
   },
+
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const group = await GroupModel.findById(id);
+
+      if (!group) {
+        res.status(404).json({ msg: "Mesa não encontrada" });
+      }
+
+      const deleteGroup = await GroupModel.findByIdAndDelete(id);
+
+      res.status(200).json({ deleteGroup, msg: "Mesa excluída com sucesso" });
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  },
+
+  update: async (req, res) => {
+    const id = req.params.id;
+
+    const group = {
+      groupName: req.body.name,
+      users: req.body.users,
+      owner: req.body.owner,
+    };
+
+    const updateGroup = await GroupModel.findByIdAndUpdate(id, group);
+
+    if (!updateGroup) {
+      res.status(404).json({ msg: "Mesa não encontrada" });
+    }
+
+    res.status(200).json({ group, msg: "Mesa editada com sucesso" });
+  },
 };
 
 module.exports = groupController;
--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -8,4 +8,10 @@ router.route("/group").get((req, res) => groupController.getAll(req, res));
 
 router.route("/group/:id").get((req, res) => groupController.get(req, res));
 
+router
+  .route("/group/:id")
+  .delete((req, res) => groupController.delete(req, res));
+
+router.route("/group/:id").put((req, res) => groupController.update(req, res));
+
 module.exports = router;
