cfae13015a2ccc2cda90fc3920920cf5a1d4d12e
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-11-20 11:07:44 -0300

    :sparkles: feat: feitas as rotas de further reading

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const forumRouter = require("./forumRoute");
 const groupRouter = require("./groupRoute");
 const forumGmRouter = require("./forumGmRoute");
+const forumReadingRouter = require("./forumReadingRoute");
 
 router.use("/", forumRouter);
 router.use("/", groupRouter);
 router.use("/", forumGmRouter);
+router.use("/", forumReadingRouter);
 
 module.exports = router;
