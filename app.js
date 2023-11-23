c235d39a34ac669b271bd21580351367a71e6913
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-11-23 11:57:03 -0300

    :construction: feat: fazendo rota de criação de usuário

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const app = express();
 
 app.use(cors());
@@ -10,6 +14,63 @@ app.use(express.json());
 const conn = require("./db/conn");
 
 conn();
+
+// Register User
+
+const User = require("./models/User");
+
+app.post("/auth/register", async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  //validações
+
+  if (!name) {
+    return res.status(422).json({ msg: "O nome é obrigatório!" });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória!" });
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(422).json({ msg: "As senhas não conferem!" });
+  }
+
+  //checa se o user existe
+
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    return res.status(422).json({ msg: "Email já cadastrado!" });
+  }
+
+  //senha
+
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  //cria usuário
+
+  const user = new User({
+    name,
+    email,
+    password,
+  });
+
+  try {
+    await user.save();
+
+    res.status(201).json({ msg: "Usuário criado com sucesso!" });
+    console.log("Usuário criado com sucesso!");
+  } catch (error) {
+    res.status(500).json({ msg: "Não é possível cadastrar no momento" });
+    console.log(error);
+  }
+});
 
 // Routes
 const routes = require("./routes/router");
--- db/conn.js
+++ db/conn.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
 async function main() {
   try {
     mongoose.set("strictQuery", true);
 
     await mongoose.connect(
-      "mongodb+srv://bulbovas:vUVjjfpSHgdc7TKc@cluster0.ij5acqp.mongodb.net/?retryWrites=true&w=majority"
+      `mongodb+srv://${dbUser}:${dbPass}@cluster0.ij5acqp.mongodb.net/?retryWrites=true&w=majority`
     );
 
     console.log("### Conectado ao Banco ###");
